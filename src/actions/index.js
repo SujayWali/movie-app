d9c02d22bae557b62a2cdc6e2c0052b29d6f287e
Sujay Wali <SujayW@ndtv.com>
2021-10-11 22:54:25 +0530

    Action Creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
-{
-    type:'ADD_MOVIES'
-    movies:[m1,m2,m2]
+// {
+//     type:'ADD_MOVIES'
+//     movies:[m1,m2,m2]
 
-}
-{
-    type:'DECREASE_COUNT'
+// }
+// {
+//     type:'DECREASE_COUNT'
+// }
+
+//Action Types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+//Action creators
+export function addMovies(movies){
+    return{
+        type: ADD_MOVIES,
+        movies
+
+    }
 }
