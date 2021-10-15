f59ecdd2a02c904f47ce433d2b87c85c3ef926ee
Sujay Wali <SujayW@ndtv.com>
2021-10-15 12:40:27 +0530

    removing from favorits [Title issue]

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,9 @@
 
 //Action Types
 export const ADD_MOVIES = 'ADD_MOVIES';
+export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
+export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
 
 //Action creators
 export function addMovies(movies){
@@ -18,3 +21,39 @@ export function addMovies(movies){
 
     }
 }
+
+export function addFavourite(movie){
+    return{
+        type: ADD_TO_FAVOURITES,
+        movie
+
+    }
+}
+
+export function removeFromFavourites(movie){
+    return{
+        type: REMOVE_FROM_FAVOURITES,
+        movie,
+
+    };
+}
+
+export function setShowFavourites(val){
+    return{
+        type: REMOVE_FROM_FAVOURITES,
+        val
+
+    };
+} 
+
+// export function addMovieToList(movie){
+//     return{
+//         type: ADD_MOVIE_TO_LIST,
+//         movie
+
+//     };
+// } 
+
+// export function handleMovieSearch(movie){
+//    const url = `http://www.omdbapi`
+// } 
