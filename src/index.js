d7dc8eda8bc9e9b057145da3e4a5d5a5894cb2db
Sujay Wali <SujayW@ndtv.com>
2021-10-18 13:57:44 +0530

    Modifying and adding middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
+//-----------Curried from of function logger (obj, next, action)----------------
+// const logger = function ({dispatch, getState}){
+//   return function (next){
+//     return function (action){
+//       //MiddleWare Code
+//       console.log("ACTION_TYPE = ",action.type);
+//       next(action);
+//     }
+//   }
+// }
+
+// ----------Another Way of writing Middleware (Modifying Middleware)-------------
+const logger = ({dispatch, getState}) => (next) => (action) => {
+    //MiddleWare Code
+    console.log("ACTION_TYPE = ",action.type);
+    next(action);
+}
+
 //Creating the store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store', store);
 // console.log('BEFORE STATE', store.getState());
 
